28f459bf8167596dba62e0dc01536878b822c88c
Diego Trejos <d.trejosh@gmail.com>
2026-04-20 11:59:05 -0600

    archivos de barril

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:objetivos/config/local_notifications/local_notifications.dart';
-import 'package:objetivos/config/theme/app_theme.dart';
 import 'package:objetivos/firebase_options.dart';
-import 'package:objetivos/infrastructure/helpers/notification_service.dart';
-import 'package:objetivos/infrastructure/helpers/shared_preferences_service.dart';
-import 'package:objetivos/presentations/providers/app_init_provider.dart';
-import 'package:objetivos/presentations/providers/notifications_service_provider.dart';
-import 'package:objetivos/presentations/screens/home_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:objetivos/data/db/isar_service.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/material.dart';
+import 'package:objetivos/config/local_notifications/local_notifications.dart';
+import 'package:objetivos/config/theme/app_theme.dart';
+
+import 'package:objetivos/presentations/screens/home_screen.dart';
+import 'package:objetivos/data/db/isar_service.dart';
+
+import 'infrastructure/helpers/helpers.dart';
+import 'presentations/providers/providers.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
